d52b0dd820f93c5df4fec6b8405996b6d3cb2277
jyotika2626 <jyotikanirban@gmail.com>
2026-05-12 14:21:54 +0530

    Create testbench.sv

---
+++ Combinational_Circuits/MUX_2x1/testbench.sv
@@ -0,0 +1,21 @@
+module tb;
+  
+reg A,B,S;
+wire Y;
+  
+mux2x1 uut(.A(A),.B(B),.S(S),.Y(Y));
+  
+initial begin
+  
+  $dumpfile("dump.vcd");
+  $dumpvars(0,tb);
+  
+  S=0; A=0; B=1; #10;
+  S=0; A=1; B=0; #10;
+  S=1; A=0; B=1; #10;
+  S=1; A=1; B=0; #10;
+  $finish;
+  
+end
+  
+endmodule
