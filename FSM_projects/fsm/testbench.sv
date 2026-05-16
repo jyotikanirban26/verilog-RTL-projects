08e65cd8c9352c45e1c29f036df4f0d8fe380764
jyotikanirban26 <jyotikanirban@gmail.com>
2026-05-16 13:54:20 +0530

    Add testbench for Moore FSM

---
+++ FSM_projects/fsm/testbench.sv
@@ -0,0 +1,29 @@
+module tb;
+  
+  reg x,clk;
+  wire z;
+  
+  moore_fsm uut(.x(x),.clk(clk),.z(z));
+  
+  initial begin
+    
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+    
+    clk = 0;
+    
+    forever #5 clk = ~clk;
+  end
+  initial begin
+    
+    x=1;  #10;
+    x=0;  #10;
+    x=1;  #10;
+    
+    x=1;  #10;
+    x=0;  #10;
+    x=1;  #10;
+    #200;
+    $finish;
+  end
+endmodule
